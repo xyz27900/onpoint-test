824b0b1873d407071f4c780202a95ee0450af401
Evgenii Kozlov <xyz27900@gmail.com>
2020-05-15 20:46:49 +0300

    Modify app.js -adding tabs component to render

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,26 @@
 import React from 'react';
+import Tabs from './tabs';
+import slide_3_1 from '../images/particles/slide-3-1.png';
+import slide_3_2 from '../images/particles/slide-3-2.png';
+import slide_3_3 from '../images/particles/slide-3-3.png';
 
-const App = () => <div>This is my app for Onpoint</div>;
+const tabsData = [
+	{
+		background: `url(${slide_3_1})`,
+		content: 'One'
+	},
+	{
+		background: `url(${slide_3_2})`,
+		content: 'Two'
+	},
+	{
+		background: `url(${slide_3_3})`,
+		content: 'Three'
+	}
+];
+
+const App = () => {
+	return <Tabs tabs={tabsData} />;
+};
 
 export default App;
