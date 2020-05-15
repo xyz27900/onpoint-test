2ed2ddfd5a6a586d7c7a675948f45f410297ac6e
Evgenii Kozlov <xyz27900@gmail.com>
2020-05-15 23:22:36 +0300

    Adding animation and render controlling to scroll down button

--- src/components/ui/scrollDownButton.js
+++ src/components/ui/scrollDownButton.js
@@ -1,18 +1,59 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { useState, useEffect } from 'react';
+import styled, { keyframes } from 'styled-components';
 import PropTypes from 'prop-types';
 import scrollDownImage from '../../images/ui/scroll-down-button.png';
+
+const fadeIn = keyframes`
+	from {
+		opacity: 0;
+	}
+
+	to {
+		opacity: 1;
+	}
+`;
+
+const fadeOut = keyframes`
+	from {
+		opacity: 1;
+	}
+
+	to {
+		opacity: 0;
+	}
+`;
 
 const NextButton = styled.button`
 	position: fixed;
 	bottom: 0;
 	display: inline-block;
 	width: 100%;
-	vertical-align: bottom
+	vertical-align: bottom;
+	animation: ${props => props.visible ? fadeIn : fadeOut} 1s ease-out;
 `;
 
-const ScrollDownButton = ({ onClick }) => <NextButton onClick={onClick}><img src={scrollDownImage} /></NextButton>;
+const ScrollDownButton = ({ isVisible = true, onClick }) => {
+	const [shouldRender, setRenderState] = useState(isVisible);
+	const handleAnimationEnd = () => {
+		!isVisible && setRenderState(false);
+	};
+
+	useEffect(() => {
+		isVisible && setRenderState(true);
+	}, [isVisible]);
+
+	return shouldRender &&
+		<NextButton
+			onClick={onClick}
+			visible={isVisible}
+			onAnimationEnd={handleAnimationEnd}>
+			<img src={scrollDownImage} />
+		</NextButton>;
+};
 
 export default ScrollDownButton;
 
-ScrollDownButton.propTypes = { onClick: PropTypes.func };
+ScrollDownButton.propTypes = {
+	isVisible: PropTypes.bool,
+	onClick: PropTypes.func
+};
