25382868faf87b3136814fc70d5b871746c07450
Evgenii Kozlov <xyz27900@gmail.com>
2020-05-16 16:35:59 +0300

    Fix border gradient

--- src/components/ui/pulseCircle.js
+++ src/components/ui/pulseCircle.js
@@ -46,19 +46,20 @@ const Circle = ({ id, x, y, radius, strokeWidth, stroke, fill, opacity }) => (
 
 const gradientColors = ['#de791b', '#cf1437'];
 const fillColor = '#f78b1f';
+const strokeGradientId = 'stroke-gradient';
 
 const PulseCircle = ({ x, y, min, max }) => {
 	return <Svg x={x} y={y} width={100} height={100} viewBox={'0 0 100 100'}>
 		<defs>
-			<linearGradient x1="0%" y1="0%" x2="0%" y2="100%">
+			<linearGradient id={strokeGradientId} x1="0%" y1="0%" x2="0%" y2="100%">
 				<stop offset="0%" stopColor={gradientColors[1]} />
 				<stop offset="100%" stopColor={gradientColors[0]} />
 			</linearGradient>
 		</defs>
-		<Circle id={0} x={50} y={50} radius={[min, max]} stroke={gradientColors} fill={fillColor} strokeWidth={1} opacity={.22} />
-		<Circle id={1} x={50} y={50} radius={[min, max]} stroke={gradientColors} fill={fillColor} strokeWidth={1} opacity={0} />
-		<Circle id={2} x={50} y={50} radius={[min, max]} stroke={gradientColors} fill={fillColor} strokeWidth={1} opacity={0} />
-		<Circle id={3} x={50} y={50} radius={[min, max]} stroke={gradientColors} fill={fillColor} strokeWidth={1} opacity={0} />
+		<Circle id={0} x={50} y={50} radius={[min, max]} stroke={`url(#${strokeGradientId})`} fill={fillColor} strokeWidth={1} opacity={.22} />
+		<Circle id={1} x={50} y={50} radius={[min, max]} stroke={`url(#${strokeGradientId})`} fill={fillColor} strokeWidth={1} opacity={0} />
+		<Circle id={2} x={50} y={50} radius={[min, max]} stroke={`url(#${strokeGradientId})`} fill={fillColor} strokeWidth={1} opacity={0} />
+		<Circle id={3} x={50} y={50} radius={[min, max]} stroke={`url(#${strokeGradientId})`} fill={fillColor} strokeWidth={1} opacity={0} />
 	</Svg>;
 };
 
