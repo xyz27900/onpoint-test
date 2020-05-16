34c0391eac7371a3c6d958be31250cd82b930e70
Evgenii Kozlov <xyz27900@gmail.com>
2020-05-16 17:21:05 +0300

    Splitting by layers and adding  parallax effect

--- src/components/scroll.js
+++ src/components/scroll.js
@@ -4,17 +4,23 @@ import styled from 'styled-components';
 import ScrollDownButton from '../components/ui/scrollDownButton';
 
 const SlideWrapper = styled.div`
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
 	transition: transform 1s ease-out;
-	transform: translateY(${props => props.shift ? props.shift : 0}%);
+	transform: translateY(${props => props.shift ? props.shift : 0}px);
 `;
 
 const Slide = styled.section`
 	position: relative;
+	top: ${props => props.top ? props.top : 0}px;
 	width: 100vw;
 	height: 100vh;
 `;
 
 const Scroll = ({ slides, currentIndex = 0, scrollSensivity = 65 }) => {
+	const parallaxSpeed = 0.15;
 	const [active, setActive] = useState(currentIndex);
 	const [startPoint, setStartPoint] = useState(null);
 	const handleTouchStart = (e) => {
@@ -39,12 +45,26 @@ const Scroll = ({ slides, currentIndex = 0, scrollSensivity = 65 }) => {
 		}
 	};
 
+
 	return <Fragment>
-		<SlideWrapper shift={active * - 100 / slides.length} onTouchStart={handleTouchStart} onTouchEnd={handleTouchEnd}>
-			{
-				slides.map((slide, index) => <Slide key={`slide-${index}`}>{slide}</Slide>)
-			}
-		</SlideWrapper>
+		{
+			slides
+				.reduce(($, row) => row.map((_, i) => [...($[i] || []), row[i]]), [])
+				.map((layer, layerIndex) => (
+					<SlideWrapper
+						key={`slide-wrapper-${layerIndex}`}
+						shift={(1 + parallaxSpeed * layerIndex) * active * - 768}
+						onTouchStart={handleTouchStart}
+						onTouchEnd={handleTouchEnd}>
+						{
+							layer.map((slide, slideIndex) => slide &&
+								<Slide
+									key={`slide-${slideIndex}`}
+									top={115 * slideIndex * (slideIndex && layerIndex)}>{slide}</Slide>) // TODO Fix '115' - it shoul be a math expression
+						}
+					</SlideWrapper>
+				))
+		}
 		<ScrollDownButton isVisible={active < slides.length - 1} onClick={slideDown} />
 	</Fragment>;
 };
@@ -54,6 +74,5 @@ export default Scroll;
 Scroll.propTypes = {
 	slides: PropTypes.arrayOf(PropTypes.node),
 	currentIndex: PropTypes.number,
-	scrollSensivity: PropTypes.number,
-	onScroll: PropTypes.func
+	scrollSensivity: PropTypes.number
 };
