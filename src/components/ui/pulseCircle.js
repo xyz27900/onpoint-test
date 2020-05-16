9bf0dae186aa489f8a9cf05df3853c62a7166944
Evgenii Kozlov <xyz27900@gmail.com>
2020-05-16 17:19:52 +0300

    Optimize render and fix prop-types

--- src/components/ui/pulseCircle.js
+++ src/components/ui/pulseCircle.js
@@ -47,8 +47,18 @@ const Circle = ({ id, x, y, radius, strokeWidth, stroke, fill, opacity }) => (
 const gradientColors = ['#de791b', '#cf1437'];
 const fillColor = '#f78b1f';
 const strokeGradientId = 'stroke-gradient';
+const circlesOpacity = [.22, 0, 0, 0];
 
 const PulseCircle = ({ x, y, min, max }) => {
+	const circleProps = {
+		x: 50,
+		y: 50,
+		radius: [min, max],
+		stroke: `url(#${strokeGradientId})`,
+		fill: fillColor,
+		strokeWidth: 1
+	};
+
 	return <Svg x={x} y={y} width={100} height={100} viewBox={'0 0 100 100'}>
 		<defs>
 			<linearGradient id={strokeGradientId} x1="0%" y1="0%" x2="0%" y2="100%">
@@ -56,10 +66,7 @@ const PulseCircle = ({ x, y, min, max }) => {
 				<stop offset="100%" stopColor={gradientColors[0]} />
 			</linearGradient>
 		</defs>
-		<Circle id={0} x={50} y={50} radius={[min, max]} stroke={`url(#${strokeGradientId})`} fill={fillColor} strokeWidth={1} opacity={.22} />
-		<Circle id={1} x={50} y={50} radius={[min, max]} stroke={`url(#${strokeGradientId})`} fill={fillColor} strokeWidth={1} opacity={0} />
-		<Circle id={2} x={50} y={50} radius={[min, max]} stroke={`url(#${strokeGradientId})`} fill={fillColor} strokeWidth={1} opacity={0} />
-		<Circle id={3} x={50} y={50} radius={[min, max]} stroke={`url(#${strokeGradientId})`} fill={fillColor} strokeWidth={1} opacity={0} />
+		{circlesOpacity.map((opacity, index) => <Circle key={`circle-${index}`} id={index} {...circleProps} opacity={opacity} />)}
 	</Svg>;
 };
 
@@ -70,7 +77,7 @@ Circle.propTypes = {
 	id: PropTypes.number,
 	x: PropTypes.number,
 	y: PropTypes.number,
-	radius: PropTypes.number,
+	radius: PropTypes.arrayOf(PropTypes.number),
 	strokeWidth: PropTypes.number,
 	stroke: PropTypes.oneOfType([PropTypes.string, PropTypes.arrayOf(PropTypes.string)]),
 	fill: PropTypes.oneOfType([PropTypes.string, PropTypes.arrayOf(PropTypes.string)]),
