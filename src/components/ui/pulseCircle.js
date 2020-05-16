858a21806f30261450167cbdba8bc0c2101d35cb
Evgenii Kozlov <xyz27900@gmail.com>
2020-05-16 16:32:11 +0300

    Remove gradient circle fill

--- src/components/ui/pulseCircle.js
+++ src/components/ui/pulseCircle.js
@@ -15,35 +15,19 @@ const circleAnimation = (min, max) => keyframes`
 		from {
 			r: ${min};
 			stroke-opacity: 0;
-			fill: url(#fill-gradient-1);
 			fill-opacity: .62
 		}
 
 		to {
 			r: ${max};
 			stroke-opacity: 1;
-			fill:url(#fill-gradient-2);
 			fill-opacity: 0;
 		}
 	`;
 
 const AnimatedCircle = styled.circle`
-	animation: ${props => circleAnimation(...props.radius)} ${props => props.id > 0 ? 2 : 0}s ${props => .95 * props.delay}s ease-out infinite;
+	animation: ${props => circleAnimation(...props.radius)} ${props => props.id > 0 ? 2 : 0}s ${props => 2 / 3 * props.id}s ease-out infinite;
 `;
-
-const Gradient = ({ type = 'linear', id, colors }) => {
-	if (type === 'linear') {
-		return <linearGradient id={id} x1="0%" y1="0%" x2="0%" y2="100%">
-			<stop offset="0%" stopColor={colors[1]} />
-			<stop offset="100%" stopColor={colors[0]} />
-		</linearGradient>;
-	} else if (type === 'radial') {
-		return <radialGradient id={id} cx="50%" cy="50%" r="50%">
-			<stop offset="28%" stopColor={colors[0]} />
-			<stop offset="100%" stopColor={colors[1]} />
-		</radialGradient>;
-	} else return null;
-};
 
 const Circle = ({ id, x, y, radius, strokeWidth, stroke, fill, opacity }) => (
 	<Fragment>
@@ -61,17 +45,20 @@ const Circle = ({ id, x, y, radius, strokeWidth, stroke, fill, opacity }) => (
 );
 
 const gradientColors = ['#de791b', '#cf1437'];
+const fillColor = '#f78b1f';
 
 const PulseCircle = ({ x, y, min, max }) => {
 	return <Svg x={x} y={y} width={100} height={100} viewBox={'0 0 100 100'}>
 		<defs>
-			<Gradient type="linear" id={'stroke-gradient'} colors={gradientColors} />
-			<Gradient type="radial" id={'fill-gradient-1'} colors={gradientColors} />
-			<Gradient type="radial" id={'fill-gradient-2'} colors={['#f78b1f', '#f78b1f']} />
+			<linearGradient x1="0%" y1="0%" x2="0%" y2="100%">
+				<stop offset="0%" stopColor={gradientColors[1]} />
+				<stop offset="100%" stopColor={gradientColors[0]} />
+			</linearGradient>
 		</defs>
-		<Circle id={0} x={50} y={50} radius={[min, max]} stroke={gradientColors} fill="url(#fill-gradient-2)" strokeWidth={1.5} opacity={.22} />
-		<Circle id={1} x={50} y={50} radius={[min, max]} stroke={gradientColors} fill="url(#fill-gradient-2)" strokeWidth={1.5} opacity={0} />
-		<Circle id={2} x={50} y={50} radius={[min, max]} stroke={gradientColors} fill="url(#fill-gradient-2)" strokeWidth={1.5} opacity={0} />
+		<Circle id={0} x={50} y={50} radius={[min, max]} stroke={gradientColors} fill={fillColor} strokeWidth={1} opacity={.22} />
+		<Circle id={1} x={50} y={50} radius={[min, max]} stroke={gradientColors} fill={fillColor} strokeWidth={1} opacity={0} />
+		<Circle id={2} x={50} y={50} radius={[min, max]} stroke={gradientColors} fill={fillColor} strokeWidth={1} opacity={0} />
+		<Circle id={3} x={50} y={50} radius={[min, max]} stroke={gradientColors} fill={fillColor} strokeWidth={1} opacity={0} />
 	</Svg>;
 };
 
