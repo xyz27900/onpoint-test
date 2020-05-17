f887f90b9deb457c4f74ce226b96bd3f8c9692a4
xyz27900 <xyz27900@gmail.com>
2020-05-17 14:12:13 +0300

    Installing image-webpack-loader for images compressing

--- webpack.config.js
+++ webpack.config.js
@@ -27,8 +27,28 @@ module.exports = {
 			},
 			{
 				test: /\.(png|jpe?g)$/,
-				loader: 'file-loader',
-				options: { name: './images/[name].[hash].[ext]' }
+				use: [
+					{
+						loader: 'file-loader',
+						options: { name: './images/[name].[hash].[ext]' }
+					},
+					{
+						loader: 'image-webpack-loader',
+						options: {
+							mozjpeg: {
+								progressive: true,
+								quality: 65
+							},
+							optipng: { enabled: false, },
+							pngquant: {
+								quality: [0.65, 0.90],
+								speed: 4
+							},
+							gifsicle: { interlaced: false, },
+							webp: { quality: 75 }
+						}
+					}
+				]
 			},
 			{
 				test: /\.(eot|svg|ttf|woff|woff2)$/,
