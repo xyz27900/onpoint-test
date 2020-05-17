438eadd7b3d7cb3650decc5559f84a8f112b3098
Evgenii Kozlov <xyz27900@gmail.com>
2020-05-17 11:09:12 +0300

    Adding Resize Observer hook

---
+++ src/components/hooks/useResizeObserver.js
@@ -0,0 +1,24 @@
+import { useRef, useLayoutEffect, useEffect, useState, useCallback } from 'react';
+
+// eslint-disable-next-line no-undef
+const useIsomorphicLayoutEffect = global.window ? useLayoutEffect : useEffect; // In case of SSR
+
+const useResizeObserver = () => {
+	const [entry, setEntry] = useState({});
+	const [element, setElement] = useState(null);
+	const observer = useRef(null);
+	const observe = useCallback(() => {
+		observer.current = new ResizeObserver(([entry]) => setEntry(entry));
+		element && observer.current.observe(element);
+	}, [element]);
+	const disconnect = useCallback(() => observer.current && observer.current.disconnect(), []);
+
+	useIsomorphicLayoutEffect(() => {
+		observe();
+		return () => disconnect();
+	}, [disconnect, observe]);
+
+	return [setElement, entry];
+};
+
+export default useResizeObserver;
