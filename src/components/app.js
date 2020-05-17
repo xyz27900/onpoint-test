2e7cec43400c4546e9ac07ac9513ef09cffa76ac
Evgenii Kozlov <xyz27900@gmail.com>
2020-05-17 10:05:47 +0300

    Change relative paths to helpers

--- src/components/app.js
+++ src/components/app.js
@@ -11,33 +11,9 @@ import slide_3_3_background from '../images/particles/slide-3-3.png';
 import slide_3_1_content from '../images/content/slide-3-1.png';
 import slide_3_2_content from '../images/content/slide-3-2.png';
 import slide_3_3_content from '../images/content/slide-3-3.png';
-import styled from 'styled-components';
 import PulseCircle from './ui/pulseCircle';
-import { colors, fonts } from './helpers';
-
-const Layer = styled.div`
-	position: relative;
-	width: 100vw;
-	height: 100vh;
-	${props => props.image && `
-		background-image: url(${props.image});
-		background-size: cover;
-		background-repeat: no-repeat;`}
-`;
-
-const TextBlock = styled.div`
-	position: absolute;
-	top: ${props => props.y}px;
-	left: ${props => props.x}px;
-	width: ${props => props.width ? props.width + 'px' : 'auto'};
-	color: ${props => props.color ? props.color : '#000000'};
-	font-family: ${props => props.fontFamily? props.fontFamily : 'sans-serif'};
-	font-size: ${props => props.fontSize? props.fontSize : '14pt'};
-	font-weight: ${props => props.fontWeight ? props.fontWeight : 400};
-	line-height: 1.2;
-	text-align: ${props => props.textAlign ? props.textAlign : 'left'};
-	letter-spacing: ${props => props.fontTracking ? props.fontTracking + 'em' : 'normal'};
-`;
+import { colors, fonts } from './helpers/utils';
+import { Layer, TextBlock } from './helpers/elements';
 
 const circlesData = [
 	{
--- src/components/ui/slider.js
+++ src/components/ui/slider.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { colors, fonts, animate } from '../helpers';
+import { colors, fonts, animate } from '../helpers/utils';
 
 const sliderThumbStyles = ({ image, imageSize }) => `
 	width: ${imageSize[0]}px;
